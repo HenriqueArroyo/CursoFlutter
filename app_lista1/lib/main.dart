a054af5620b4899255192d082b55450f933091c7
HenriqueArroyo <ana123ca.henrique@gmail.com>
2024-02-22 16:56:23 -0300

    22/02

---
+++ app_lista1/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:app_lista1/tarefaModel.dart';
+
+void main() {
+
+}
+
--- jogo_sorteio/android/app/src/main/kotlin/com/example/jogo_sorteio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jogo_sorteio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
