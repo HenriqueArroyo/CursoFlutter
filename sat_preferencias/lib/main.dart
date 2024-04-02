c80e322d4df064f301675cf071c1c894318bdcad
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-04-02 13:45:04 -0300

    02/04

--- sat_preferencias/lib/main.dart
+++ sat_preferencias/lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'login.dart';
+import 'registro.dart';
+import 'settings_page.dart';
 
-void main(){
-  runApp(myApp());
+
+void main() {
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Meu App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else {
+          if (snapshot.hasData) {
+            return SettingsPage();
+          } else {
+            return LoginPage();
+          }
+        }
+      },
+    );
   }
 }
