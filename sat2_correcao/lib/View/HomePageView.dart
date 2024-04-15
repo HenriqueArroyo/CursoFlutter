86b69100fe9d9998cfba1898f86abaa44a7a8f9c
HenriqueArroyo <ana123ca.henrique@gmail.com>
2024-04-15 15:16:52 -0300

    15/04

---
+++ sat2_correcao/lib/View/HomePageView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaginaHome extends StatefulWidget {
+  const PaginaHome({super.key});
+
+  @override
+  State<PaginaHome> createState() => _PaginaHomeState();
+}
+
+class _PaginaHomeState extends State<PaginaHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- sat2_correcao/lib/View/LoginPageView.dart
+++ sat2_correcao/lib/View/LoginPageView.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../Controller/BancoDados.dart';
+import '../Model/Usuario.dart';
 
 class PaginaLogin extends StatefulWidget {
   const PaginaLogin({super.key});
@@ -12,6 +16,80 @@ class _PaginaLoginState extends State<PaginaLogin> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _senhaController = TextEditingController();
   bool _loading = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela de Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Por favor, insira seu e-mail';
+                    } else if (!isValidEmail(value)) {
+                      return 'E-mail inválido';
+                    }
+                    return null;
+                  },
+                  inputFormatters: [
+                    FilteringTextInputFormatter.deny(RegExp(r'[0-9]')),
+                  ],
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _senhaController,
+                  decoration: InputDecoration(labelText: 'Senha'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Por favor, insira sua senha';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                _loading
+                    ? CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: _login,
+                        child: Text('Acessar'),
+                      ),
+                SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PaginaCadastro()),
+                    );
+                  },
+                  child: Text('Não tem uma conta? Cadastre-se'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   void _login() async {
     if (_formKey.currentState!.validate()) {
@@ -24,12 +102,12 @@ class _PaginaLoginState extends State<PaginaLogin> {
 
       BancoDadosCrud bancoDados = BancoDadosCrud();
       try {
-       Usuario usuario = await bancoDados.getUsuario(email, senha);
+        Usuario? usuario = await bancoDados.getUsuario(email, senha);
         if (usuario != null) {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => ConfiguracoesPage(email: usuario.email),
+              builder: (context) => PaginaConfiguracoes(email: user.email),
             ),
           );
         } else {
@@ -48,79 +126,6 @@ class _PaginaLoginState extends State<PaginaLogin> {
         });
       }
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-      title: Text("Tela de Login");,
-     ) ,
-    body:Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'E-mail'),
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Por favor, insira seu e-mail';
-                  } else if (!isValidEmail(value)) {
-                    return 'E-mail inválido';
-                  }
-                  return null;
-                },
-                inputFormatters: [
-                  FilteringTextInputFormatter.deny(RegExp(r'[0-9]')),
-                ],
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: _senhaController,
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
-                validator: (value) {
-                  if (value?.trim().isEmpty ?? true) {
-                    return 'Por favor, insira sua senha';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              _loading
-                  ? CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: _login,
-                      child: Text('Acessar'),
-                    ),
-              SizedBox(height: 20),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CadastroScreen()),
-                  );
-                },
-                child: Text('Não tem uma conta? Cadastre-se'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ););
   }
 
   bool isValidEmail(String email) {
